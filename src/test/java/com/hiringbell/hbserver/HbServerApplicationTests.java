33a49f65250b4cfa28d68404cfd0b2f365ddba88
istiyakmi9 <istiyaq.mi9@gmail.com>
2023-12-01 11:14:47 +0530

    server code

---
+++ src/test/java/com/hiringbell/hbserver/HbServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiringbell.hbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
