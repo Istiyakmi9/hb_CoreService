b939b92820e92b24f48ece538cf58f337c59bbd9
sarfaraznawaz35 <sarfaraznawaz35@gmail.com>
2023-12-21 16:48:45 +0530

    JwtGateway and jwt token has been generated

--- src/main/java/com/hiringbell/hbserver/controller/EmployeeController.java
+++ src/main/java/com/hiringbell/hbserver/controller/EmployeeController.java
@@ -1,13 +1,18 @@
 package com.hiringbell.hbserver.controller;
 
 import com.hiringbell.hbserver.entity.Employee;
+import com.hiringbell.hbserver.jwtconfig.JwtGateway;
 import com.hiringbell.hbserver.model.ApiResponse;
+import com.hiringbell.hbserver.model.JwtTokenModel;
 import com.hiringbell.hbserver.serviceImpl.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/employee")
@@ -22,7 +27,12 @@ public class EmployeeController {
         return ResponseEntity.ok(ApiResponse.Ok(null));
     }
 
-
+    @PostMapping("/testGenerateJwtToken")
+    public ResponseEntity<ApiResponse> testGenerateJwtToken(@RequestBody JwtTokenModel jwtTokenModel) throws IOException {
+        JwtGateway jwtGateway = JwtGateway.getJwtGateway();
+        String result = jwtGateway.generateJwtToken(jwtTokenModel);
+        return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
 
 
 }
