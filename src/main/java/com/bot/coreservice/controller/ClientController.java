13e7e96637eda342ad7cda82a1fa88afc5c7e02f
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-01-10 16:55:57 +0530

    path improved

--- src/main/java/com/bot/coreservice/controller/ClientController.java
+++ src/main/java/com/bot/coreservice/controller/ClientController.java
@@ -9,25 +9,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/hb/api/client")
+@RequestMapping("/hb/api/client/")
 public class ClientController {
     @Autowired
     ClientService clientService;
-    @PostMapping("/getAllClient")
+    @PostMapping("getAllClient")
     public ResponseEntity<ApiResponse> getAllClient(@RequestBody FilterModel filter) throws Exception {
         var result = clientService.getAllClientService(filter);
         return ResponseEntity.ok(ApiResponse.Ok(result));
     }
 
-    @PostMapping("/manageClient")
+    @PostMapping("manageClient")
     public ResponseEntity<ApiResponse> manageClient(@RequestBody Client client) throws Exception {
         var result = clientService.mangeClientService(client);
         return ResponseEntity.ok(ApiResponse.Ok(result));
     }
 
-    @GetMapping("/getClientById/{clientId}")
+    @GetMapping("getClientById/{clientId}")
     public ResponseEntity<ApiResponse> getClientById(@PathVariable("clientId") int clientId) throws Exception {
         var result = clientService.getClientService(clientId);
         return ResponseEntity.ok(ApiResponse.Ok(result));
+    }
+
+    @GetMapping("test")
+    public String test(){
+        return  "Successfully";
     }
 }
